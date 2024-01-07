e687e533852f3d7e5fff1f290e723da187dfab88
Simon Wood <simon@mungewell.org>
2024-01-06 19:20:33 -0700

    Add templates to build FLST_SEQ.ZT2 from scratch

---
+++ zoom_fx_AllZDL7/B1 FOUR/unzipped/FLST_SEQ.sh
@@ -0,0 +1,93 @@
+python3 zoomzt2.py -i  0x1000030 -A SLWATK.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000040 -A ZNR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000080 -A OPTCOMP.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800010 -A BLACKOPT.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800020 -A LMT1176.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800030 -A 160_COMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800040 -N -A DUAL_CMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800050 -A MB_COMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800060 -A DCOMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800070 -A GLAMCOMP.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000040 -A SEQFLTR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x20000b0 -A EXCITER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800010 -A B_ATWAH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800020 -A Z_TRON.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800030 -N -A A_FILTER.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800040 -A BA_CRY.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800051 -A BA_GEQ1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800061 -N -A STBGEQ1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800070 -A BA_PEQ.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800080 -A SPLITTER.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800090 -N -A LOW_EQ.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2800095 -A HIGH_EQ.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x28000a0 -N -A ENV_FLTR.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000020 -A EP_STOMP.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000030 -N -A RCBOOST.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000d0 -A NYC_MUFF.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000130 -A SQUEAK.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800011 -A BA_DRV1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800021 -A DIPLUS1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800031 -A DRKPRE1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800040 -A BASS_BB.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800050 -A DI5.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800060 -A BA_PRE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800070 -N -A B_OD.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800080 -A BA_TSDRV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3800090 -A DARK_OD.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x38000a0 -A BBB_OD.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x38000b0 -A VOODOO_B.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x38000c0 -N -A BA_FZSML.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x38000d0 -N -A BA_METAL.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x38000e0 -A BA_OCTFZ.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800011 -A SVT_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800021 -A BMN1001U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800031 -A SWR4001U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800041 -A AG750_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800051 -A TE400_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800061 -A AC370_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800070 -A MINIMARK.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800081 -A EBH3601U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4800091 -A B15N_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800010 -A SVT_8X10.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800018 -A SV4X10TW.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800020 -A FD_B4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800030 -A SWR_4X10.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800040 -A AG4X10TW.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800050 -A TE_4X10.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800060 -A AC_1X18.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800070 -A MKB2X8TW.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800080 -A EB4X10TW.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5800090 -A AM1X15.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000010 -A TREMOLO.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000040 -A PHASER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000060 -N -A THEVIBE.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000b0 -A PITCHSHF.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000d0 -N -A HPS.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000100 -A CLONECHO.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000110 -A SUPERCHO.ZD2 -v 1.40 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000131 -A CRNTRI3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800020 -A B_ST_CHO.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800040 -A B_VFLNG.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800050 -A B_OCTAVE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800060 -A B_DETUNE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800070 -A B_PITCH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6800080 -N -A BASS_PHA.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000010 -A BOMBER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000041 -N -A LPROLL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7800010 -N -A STDSYN.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7800020 -A SYNTLK.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7800031 -A Z_SYN_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7800040 -A DEFRET.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7800050 -A PH_DIST.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000011 -A DELAY_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000021 -A ANDLY_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000030 -A TAPEECHO.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000041 -N -A RVRSDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000050 -N -A MODDELAY.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000a1 -N -A DUALDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000010 -N -A AIR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000020 -A ROOM.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000030 -N -A HALL.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000040 -A HD_HALL.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000050 -A SPRING.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000070 -A PLATEREV.ZD2 -v 1.30 -w FLST_SEQ.ZT2
---
+++ zoom_fx_AllZDL7/G1X FOUR/unzipped/FLST_SEQ.sh
@@ -0,0 +1,148 @@
+python3 zoomzt2.py -i  0x1000010 -A COMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000020 -A RACKCOMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000030 -A SLWATK.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000040 -A ZNR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000050 -N -A MUTESW.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000060 -A GRAYCOMP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000070 -N -A N_GATE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1000080 -N -A OPTCOMP.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800010 -N -A BLACKOPT.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x1800020 -A LMT1176.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000010 -A AUTOWAH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000020 -N -A RESONANC.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000030 -N -A CRY.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000040 -A SEQFLTR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000051 -N -A GT_GEQ1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000054 -A GTGEQ71U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000061 -N -A STGTGE1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000070 -A PARAEQ.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x2000090 -A RNDMFLTR.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x20000a0 -N -A LOWPASSF.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x20000b0 -N -A EXCITER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x20000c0 -N -A STEP.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x20000d0 -N -A LFO_FLTR.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000010 -A TS_DRIVE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000020 -A EP_STOMP.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000030 -A RCBOOST.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000040 -A GOLD_DRV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000050 -A SWEETDRV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000060 -A DYNDRIVE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000070 -A REDCRUNC.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000080 -N -A MTLWRLD.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000090 -N -A TB_MK15.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000a0 -N -A OCTFUZZ.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000b0 -N -A SPTBOOST.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000c0 -A ACOSIM.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000d0 -A NYC_MUFF.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000e0 -A BGTHRTTL.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x30000f0 -N -A BG_GRID.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000120 -A DIST_1.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000130 -A SQUEAK.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000140 -N -A UPOCTBST.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000150 -N -A OUT_BST.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000160 -A DISTPLUS.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000170 -A ZEN_DRV.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x3000190 -A VIOLETDS.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000011 -A MS800_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000019 -A MS19591U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x400001b -N -A MS45OS1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000021 -A FD_TWR1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000028 -A FDBMAN1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x400002c -A FDDLXR1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x400002d -N -A FDMSTR1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000031 -A UK30A_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000041 -A BGMK1_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000043 -N -A BGMK3_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000051 -A XTSBLU1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000061 -A HW100_1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000071 -A RCTORG1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000081 -A ORG1201U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x4000091 -A DZ_DRV1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x40000a1 -A MACH301U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000010 -A MS4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000018 -A MS4X12GB.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x500001a -N -A MS4X12AL.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000020 -A FD2X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000027 -A FDB4X10.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x500002a -A FDDX1X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x500002b -N -A FDMA2X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000030 -A UK2X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000040 -A MK1_1X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000042 -N -A MK3_1X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000050 -A BGN4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000060 -A HW4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000070 -A RCT4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000080 -A ORG_4X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x5000090 -A DZ4X12F.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x50000a0 -A MA2X12.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000010 -A TREMOLO.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000020 -N -A CHORUS.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000030 -A STEREOCH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000040 -A PHASER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000050 -A VINFLNGR.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000060 -N -A THEVIBE.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000070 -A VIBRATO.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000080 -A OCTAVE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000090 -N -A RINGMOD.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000a0 -N -A DETUNE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000b0 -A PITCHSHF.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000c0 -N -A MONOPITC.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000d0 -A HPS.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x60000f0 -N -A SLICER.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000100 -A CLONECHO.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000110 -N -A SUPERCHO.ZD2 -v 1.40 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000120 -N -A STONEPHA.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000131 -A CRNTRI3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000140 -N -A BENDCHO.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000150 -N -A ANA234CH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000160 -N -A WRPPHASE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x6000171 -N -A DUOPHA1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000010 -A BOMBER.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000020 -N -A AUTOPAN.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000041 -N -A LPROLL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x7000050 -A HOTSPICE.ZD2 -v 1.10 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000011 -A DELAY_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000021 -A ANDLY_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000030 -A TAPEECHO.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000041 -A RVRSDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000050 -A MODDELAY.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000081 -A PPDLY_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000090 -N -A FLTRDLY.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000a1 -N -A DUALDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000b1 -N -A PTCHDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000c1 -N -A SLPBKD3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000d1 -N -A APANDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000e1 -N -A PHASDL1U.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x80000f1 -N -A TPEC3_3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000101 -N -A ICEDLY3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000111 -N -A SLATDL3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x8000121 -A SOFTEC3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000010 -N -A AIR.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000020 -A ROOM.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000030 -A HALL.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000040 -A HD_HALL.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000050 -A SPRING.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000060 -A FDSPRING.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000070 -A PLATEREV.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x9000080 -N -A EARLYREF.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x90000a0 -N -A CHURCH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x90000d0 -N -A CHAMBER.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0x90000e0 -N -A GATE_REV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000010 -A PDL_VOL.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000020 -A BLCK_WAH.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000030 -N -A CHRM_WAH.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000040 -N -A WH100.ZD2 -v 1.30 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000050 -A PDL_PIT.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000060 -N -A PDL_MNPT.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000070 -A PDL_VIBE.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000080 -N -A PDL_DRV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000090 -N -A PDL_PHA.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000a1 -A PDLDLY3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000b0 -A PDL_REV.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000c1 -N -A OSCECH3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000d0 -N -A VOICE_WH.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000e1 -N -A PDLROT3S.ZD2 -v 1.00 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb0000f0 -N -A PDL_BITC.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb000100 -N -A PDL_FLNG.ZD2 -v 1.20 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xb800020 -N -A PDL_RESO.ZD2 -v 1.40 -w FLST_SEQ.ZT2
+python3 zoomzt2.py -i  0xbfffff0 -A OUT_VP.ZD2 -v 1.10 -w FLST_SEQ.ZT2
