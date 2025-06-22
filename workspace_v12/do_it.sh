a54472ba990b7e171d87cac36b07f99e1fc9eb57
Simon Wood <simon@mungewell.org>
2025-06-21 21:18:00 -0600

    Scripting changes

--- workspace_v12/do_it.sh
+++ workspace_v12/do_it.sh
@@ -8,20 +8,24 @@ export dis6x=~/ti/ccs1281/ccs/tools/compiler/ti-cgt-c6000_8.3.12/bin/dis6x
 # 	check whether any symbols are missed (ie not word aligned)
 # 	add MD5Sum/Name of source effect's '.code'
 
-find . -name '*.code' -exec bash -c "readelf -W -r {} > {}.reloc; \
+find . -name '*.code' -exec bash -c " \
+	readelf -W -r {} > {}.reloc; \
 	$dis6x --all --noaddr --realquiet {} > temp.asm; \
 	\
-	$dis6x --all --bytes --noaddr --realquiet --suppress {} | \
-	grep -e \":$\" > 1.temp; \
-	grep -e \":$\" {}.asm | grep -v -e \"^;.*:$\" > 2.temp; \
-	diff -q 1.temp 2.temp >> {}.asm; \
-	\
-	echo    '                    zoomzt2:' >> temp.asm; \
-	echo -n '                            .cstring \"' >> temp.asm; \
+	echo -e '\n                    zoomzt2:' >> temp.asm; \
+	echo -n '                              .cstring \"' >> temp.asm; \
 	md5sum {} | tr -d '\n' >> temp.asm; \
 	echo '\"' >> temp.asm; \
 	\
-	python3 patch_asm.py -r {}.reloc temp.asm > {}.asm;" \;
+	python3 patch_asm.py -r {}.reloc temp.asm > {}.asm; \
+	\
+	$dis6x --all --bytes --noaddr --realquiet --suppress {} | \
+	grep -e \":$\" > 1.temp; \
+	grep -e \":$\" {}.asm | grep -v -e \"^;.*:$\" | grep -v \"zoomzt2\" > 2.temp; \
+	diff -q 1.temp 2.temp > 3.temp; if [ -s 3.temp ];then \
+	echo -e '\n.error \"symbols are not word aligned!\"' >> {}.asm; \
+	fi; \
+	" \;
 
 
-rm -f temp.asm 1.temp 2.temp
+rm -f temp.asm 1.temp 2.temp 3.temp
