b947429649e0aef94256ffd8fec2562f3e345183
Simon Wood <simon@mungewell.org>
2025-06-23 18:28:07 -0600

    Script improvement

--- workspace_v12/do_it.sh
+++ workspace_v12/do_it.sh
@@ -1,5 +1,16 @@
+# Script to automate disassembly of ZD2 code segements and combine it with
+# relocation table to aid editting/re-compilation
+#
+# Extract 'code' section first, ie:
+# $ python3 zoomzt2/decode_effect.py -c LINESEL.ZD2.code LINESEL.ZD2
 
-echo processing all '.code' files
+if [ -z ${1+x} ]; then
+	echo processing all '.code' files
+	export target="*.code"
+else
+	echo processing $1
+	export target=$1
+fi
 
 export dis6x=~/ti/ccs1281/ccs/tools/compiler/ti-cgt-c6000_8.3.12/bin/dis6x
 
@@ -8,7 +19,7 @@ export dis6x=~/ti/ccs1281/ccs/tools/compiler/ti-cgt-c6000_8.3.12/bin/dis6x
 # 	check whether any symbols are missed (ie not word aligned)
 # 	add MD5Sum/Name of source effect's '.code'
 
-find . -name '*.code' -exec bash -c " \
+find . -name "$target" -exec bash -c " \
 	readelf -W -r {} > {}.reloc; \
 	$dis6x --all --noaddr --realquiet {} > temp.asm; \
 	\
@@ -23,7 +34,7 @@ find . -name '*.code' -exec bash -c " \
 	grep -e \":$\" > 1.temp; \
 	grep -e \":$\" {}.asm | grep -v -e \"^;.*:$\" | grep -v \"zoomzt2\" > 2.temp; \
 	diff -q 1.temp 2.temp > 3.temp; if [ -s 3.temp ];then \
-	echo -e '\n.error \"symbols are not word aligned!\"' >> {}.asm; \
+	echo -e '\n.emsg \"symbols are not word aligned!\"' >> {}.asm; \
 	fi; \
 	" \;
 
