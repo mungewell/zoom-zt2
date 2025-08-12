938792f5271525fd035cb685583e3cd51f1484e1
Simon Wood <simon@mungewell.org>
2025-08-11 19:07:33 -0600

    Fix '.bat' to check for pre-built, or attempt to run Python

--- zoomzt2-gui.bat
+++ zoomzt2-gui.bat
@@ -1,3 +1,12 @@
-REM redirect to prebuilt/py2exe file
+echo off
+REM script to run the GUI version of ZoomZT2
 
-build\exe.win-amd64-3.12\zoomzt2-gui.exe %*
+if exist build\exe.win-amd64-3.12\zoomzt2-gui.exe (
+	echo Found 'pre-built/py2exe' file
+	build\exe.win-amd64-3.12\zoomzt2-gui.exe %*
+) else (
+	echo Trying to run Python script directly
+	echo (note: system may be missing some required modules)
+	py.exe zoomzt2-gui.py
+)
+
